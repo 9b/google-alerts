0bf1656d554448bab95a31d7229645242d0351a9
9b <brandon@9bplus.com>
2018-07-10 20:51:06 -0400

    Bump the rev.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='google-alerts',
-    version='0.1.6',
+    version='0.1.7',
     description='Abstraction to manage Google Alerts from code',
     url="https://github.com/9b/google-alerts",
     author="Brandon Dixon",
